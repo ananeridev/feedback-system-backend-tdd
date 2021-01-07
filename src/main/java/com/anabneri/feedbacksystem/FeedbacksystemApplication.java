bf5df773f516b35fc0c4b6084b77671a8dd105ce
anabneri <$git_config_user_email>
2021-01-07 11:39:31 -0300

    feat: init

---
+++ src/main/java/com/anabneri/feedbacksystem/FeedbacksystemApplication.java
@@ -0,0 +1,13 @@
+package com.anabneri.feedbacksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbacksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbacksystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anabneri/feedbacksystem/FeedbacksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabneri.feedbacksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbacksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
