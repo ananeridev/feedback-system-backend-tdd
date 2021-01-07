e9dc9e3958b9a8e7b6bd1e5738be0a9bd23ed1dc
anabneri <$git_config_user_email>
2021-01-07 17:19:30 -0300

    feat(controller test): creating the first method test

--- src/test/java/com/anabneri/feedbacksystem/controller/FeedbackControllerTest.java
+++ src/test/java/com/anabneri/feedbacksystem/controller/FeedbackControllerTest.java
@@ -1,11 +1,36 @@
 package com.anabneri.feedbacksystem.controller;
 
+import com.anabneri.feedbacksystem.model.EmployeeEntry;
+import com.anabneri.feedbacksystem.model.FeedbackEmployee;
 import com.anabneri.feedbacksystem.service.FeedbackService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+import java.util.TimeZone;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -14,4 +39,52 @@ public class FeedbackControllerTest {
 
     @MockBean
     private FeedbackService service;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // create the DateFormat to set a Date and Hour that I can use to compare SpringMvc returned the dates to expected values
+    private static DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+    @BeforeAll
+    static void beforeAll() {
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+    }
+
+    @Test
+    @DisplayName("GET /feedback/feedbackId - THE FEEDBACK WAS FOUND")
+    void testFeedbackByIdFound() throws Exception {
+
+        FeedbackEmployee mockEmployee = new FeedbackEmployee("1","Jacksson",1);
+        Date now = new Date();
+        mockEmployee.getEntries().add(new EmployeeEntry("joana_from_HR",now,
+                "Jacksson it's a great employee, very insightful"));
+
+
+
+    }
+
+    @Test
+    @DisplayName("GET /feedback/feedbackId - THE FEEDBACK WAS NOT FOUND")
+    void testFeedbackByIdNotFound() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("POST /feedback - SUCCESS")
+    void testFeedback() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("GET /feedback/{employeeId}/entry - FEEDBACK BY EMPLOYEE ENTRY")
+    void testFeedbackByIdFound() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("GET /feedback/{employeeId}/entry - FEEDBACK BY EMPLOYEE ENTRY")
+    void testFeedbackByIdFound() throws Exception {
+
+    }
 }
