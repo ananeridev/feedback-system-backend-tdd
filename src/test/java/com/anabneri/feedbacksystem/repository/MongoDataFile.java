7fa834538d2e79e4024e2cf07e7cad9b1fcd0e17
anabneri <$git_config_user_email>
2021-01-08 16:47:13 -0300

    feat(): creating MongoSpringExtension for tests on repository'

--- src/test/java/com/anabneri/feedbacksystem/repository/MongoDataFile.java
+++ src/test/java/com/anabneri/feedbacksystem/repository/MongoDataFile.java
@@ -1,4 +1,29 @@
 package com.anabneri.feedbacksystem.repository;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MongoDataFile {
+
+    /**
+     * The name of the MongoDB JSON test file.
+     * @return the name of the MongoDB JSON test file.
+     * */
+    String value();
+
+    /**
+     * The class of objects stored in the MongoDB test file.
+     * @return The class of objects stored in the MongoDB test file.
+     * */
+    Class classType();
+
+    /**
+     * The name of the MongoDB collection hosting the test objects.
+     * @return The name of MongoDB collection hosting the test objects.
+     * */
+    String collectionName();
 }
--- src/test/java/com/anabneri/feedbacksystem/repository/MongoSpringExtension.java
+++ src/test/java/com/anabneri/feedbacksystem/repository/MongoSpringExtension.java
@@ -1,29 +1,99 @@
 package com.anabneri.feedbacksystem.repository;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
-@Target({ElementType.METHOD, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MongoSpringExtension {
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+public class MongoSpringExtension implements BeforeEachCallback, AfterEachCallback {
+
+    // path to where out test JSON files are stored
+    private static Path JSON_PATH = Paths.get("src", "test", "resources","data");
+
+    // jackson objectMapper: used to load a JSON file into a list of objects
+    private ObjectMapper mapper = new ObjectMapper();
 
     /**
-     * The name of the MongoDB JSON test file.
-     * @return the name of the MongoDB JSON test file.
+     * Called before each test executes. This callback is responsible for importing the JSON document, defined
+     * by the MongoDataFile annotation, into the embedded MongoDB, thought the provided MongoTemplate
+     *
+     * @param context   The ExtensionContext, which provides access to the test method.
+     * @throws Exception If an error occurs retrieving the test method or extracting the MongoDataFile annotation.
+     *
      * */
-    String value();
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        context.getTestMethod().ifPresent(method -> {
+            // load test file from the annotation
+            MongoDataFile mongoDataFile = method.getAnnotation(MongoDataFile.class);
+
+
+            // load the MongoTemplate that we can use to import our data
+            getMongoTemplate(context).ifPresent(mongoTemplate -> {
+                try {
+                    // use jackson's objectmapper to load a list of objects from the JSON file
+                    List objects = mapper.readValue(JSON_PATH.resolve(mongoDataFile.value()).toFile(),
+                            mapper.getTypeFactory().constructCollectionType(List.class, mongoDataFile.classType()));
+
+                    // save each object into MongoDB
+                    objects.forEach(mongoTemplate::save);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            });
+        });
+    }
 
     /**
-     * The class of objects stored in the MongoDB test file.
-     * @return The class of objects stored in the MongoDB test file.
-     * */
-    Class classType();
+     * Called after each test executes. This callback is responsible for dropping the test's MongoDB collection
+     * so that the next test that runs is clean.
+     *
+     * @param context       The ExtensionContext, which provides access to the test method.
+     * @throws Exception    If an error occurs retrieving the test method or extracting the MongoDataFile annotation.
+     */
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        context.getTestMethod().ifPresent(method -> {
+            // load the MongoDataFile annotation value from the test method
+            MongoDataFile mongoDataFile = method.getAnnotation(MongoDataFile.class);
+
+            // load the MongoTemplate that we can use to drop the test class collection
+            Optional<MongoTemplate> mongoTemplate = getMongoTemplate(context);
+            mongoTemplate.ifPresent(t -> t.dropCollection(mongoDataFile.collectionName()));
+        });
+    }
+
 
     /**
-     * The name of the MongoDB collection hosting the test objects.
-     * @return The name of MongoDB collection hosting the test objects.
-     * */
-    String collectionName();
+     * Helper method that uses reflection to invoke the getMongoTemplate() method on the test instance.
+     * @param context   The ExtensionContext, which provides access to the test instance.
+     * @return          An optional MongoTemplate, if it exists.
+     */
+    private Optional<MongoTemplate> getMongoTemplate(ExtensionContext context) {
+        Optional<Class<?>> clazz = context.getTestClass();
+        if(clazz.isPresent()) {
+            Class<?> c = clazz.get();
+            try {
+                // find the getMongoTemplate method on the test class
+                Method method = c.getMethod("getMongoTemplate",null);
+
+                // invoke the getMongoTemplate method on the test class
+                Optional<Object> testInstance = context.getTestInstance();
+                if (testInstance.isPresent()) {
+                    return Optional.of((MongoTemplate)method.invoke(testInstance.get(),null));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return Optional.empty();
+    }
 }
